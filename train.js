a75e978897367f151bfdbf5bc2705271a70e9a0a
dburnet3 <deniseburnett26@gmail.com>
2019-05-08 09:37:49 -0400

    here are my files for the project.  Had some issues with firebase

---
+++ train.js
@@ -0,0 +1,92 @@
+// $(document).ready(function () {
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#config-web-app -->
+
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBHjPzxfSjcsk1WOA1t9rXx1hj902wkB-w",
+    authDomain: "train-6d9c0.firebaseapp.com",
+    databaseURL: "https://train-6d9c0.firebaseio.com",
+    projectId: "train-6d9c0",
+    storageBucket: "train-6d9c0.appspot.com",
+    messagingSenderId: "1060692871277",
+    appId: "1:1060692871277:web:0f4ceb8a06b5a0c6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+// var config = {
+//     apiKey: "AIzaSyBgxvUw0se9Lvme8ia5JyhZXLPHC2YUeBI",
+//     authDomain: "trainproject-b8986.firebaseapp.com",
+//     databaseURL: "https://trainproject-b8986.firebaseio.com",
+//     projectId: "trainproject-b8986",
+//     storageBucket: "trainproject-b8986.appspot.com",
+//     messagingSenderId: "441344714402"
+// };
+// firebase.initializeApp(config);
+
+// A variable to reference the database.
+var database = firebase.database();
+
+// Variables for the onClick event
+var name = "";
+var destination = "";
+var firstTrain = "";
+var frequency = 0;
+
+$("#add-train").on("click", function () {
+    event.preventDefault();
+    // Storing and retreiving new train data
+    name = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    firstTrain = $("#first-train").val().trim();
+    frequency = $("#frequency").val().trim();
+
+    // Pushing to database
+    database.ref().push({
+        name: name,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+    $("form")[0].reset();
+});
+
+database.ref().on("child_added", function (childSnapshot) {
+    var nextArr;
+    var minAway;
+    // Chang year so first train comes before now
+    var firstTrainNew = moment(childSnapshot.val().firstTrain, "hh:mm").subtract(1, "years");
+    // Difference between the current and firstTrain
+    var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+    var remainder = diffTime % childSnapshot.val().frequency;
+    // Minutes until next train
+    var minAway = childSnapshot.val().frequency - remainder;
+    // Next train time
+    var nextTrain = moment().add(minAway, "minutes");
+    nextTrain = moment(nextTrain).format("hh:mm");
+
+    $("#add-row").append("<tr><td>" + childSnapshot.val().name +
+        "</td><td>" + childSnapshot.val().destination +
+        "</td><td>" + childSnapshot.val().frequency +
+        "</td><td>" + nextTrain +
+        "</td><td>" + minAway + "</td></tr>");
+
+    // Handle the errors
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
+
+
+// });
+
+
+
+
